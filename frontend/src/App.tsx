e8197162849d5c8426d30a8b025db001e175a68b
Timothy Zucker <155255765+zuck30@users.noreply.github.com>
2026-02-24 03:00:18 +0300

    zuck created file

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatInterface from './components/Mazungumzo/ChatInterface';
+import './styles/kitenge-theme.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-amber-50">
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
